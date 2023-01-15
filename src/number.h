2ef67985453e9d77da71aa63f87b0f7484dfa190
Hutecki <hubert0706@wp.pl>
2023-01-15 17:01:14 +0100

    numberh

--- src/number.h
+++ src/number.h
@@ -1,3 +1,11 @@
+#include <functional>
+
+// number type used throughout the program
+using Number = double;
+
+
+
+
 // function prototype used by every arithmetic functions
 using NumberOperation = std::function<Number(Number, Number)>;
 
