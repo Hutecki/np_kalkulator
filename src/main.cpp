56888d19ca6b2d6a351cf23305acfa3a8f5f5119
nope <116457627+0verdoze@users.noreply.github.com>
2023-01-15 08:55:40 +0100

    add reading user data, and evaluating it

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <optional>
 #include "number.h"
 
 std::vector<std::pair<std::string, NumberOperation>> OPERATIONS;
 
+struct UserInput {
+    NumberOperation op;
+    Number number_a, number_b;
+};
 
 void display_user_interface() {
     std::cout << 
@@ -21,8 +26,54 @@ void display_user_interface() {
     }
 }
 
+std::optional<UserInput> read_user_input() {
+    size_t idx;
+    Number a, b;
+
+    std::cout << "Podaj numer operacji: ";
+    if (!(std::cin >> idx && idx - 1 < OPERATIONS.size())) {
+        return std::nullopt;
+    }
+
+    std::cout << "Podaj pierwsza liczbe: ";
+    if (!(std::cin >> a)) {
+        return std::nullopt;
+    }
+
+    std::cout << "Podaj druga liczbe: ";
+    if (!(std::cin >> b)) {
+        return std::nullopt;
+    }
+
+    return UserInput {
+        OPERATIONS[idx - 1].second,
+        a,
+        b
+    };
+}
+
+Number eval_user_input(const UserInput& input) {
+    return input.op(input.number_a, input.number_b);
+}
+
+void handle_user_interaction() {
+    while (true) {
+        auto input_opt = read_user_input();
+
+        if (input_opt.has_value()) {
+            Number result = eval_user_input(*input_opt);
+            std::cout << "Wynik wynosi: "
+                << result
+                << '\n';
+
+            return;
+        }
+    }
+}
+
 int main(int argc, char* argv[]) {
     display_user_interface();
+    handle_user_interaction();
 
     return 0;
 }
