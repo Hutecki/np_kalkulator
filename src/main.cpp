890a547e841ec11463d78a3b419ce3d61132c790
nope <116457627+0verdoze@users.noreply.github.com>
2023-01-15 08:29:21 +0100

    Add user prompt

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 #include <string>
 #include "number.h"
@@ -5,6 +6,23 @@
 std::vector<std::pair<std::string, NumberOperation>> OPERATIONS;
 
 
+void display_user_interface() {
+    std::cout << 
+        "Witaj użytkowniku\n"
+        "Masz do wyboru "
+        << OPERATIONS.size()
+        << " operacje\n";
+
+    for (size_t i = 0; i < OPERATIONS.size(); i++) {
+        std::cout << i + 1
+            << ". "
+            << OPERATIONS[i].first
+            << "\n";
+    }
+}
+
 int main(int argc, char* argv[]) {
+    display_user_interface();
+
     return 0;
 }
